ba57643e99ed61745749bc51d1b9ffde055d1028
rumbodeveloper <rumbo.developer@rumbo181.com>
2018-01-22 23:08:46 +0100

    Nueva funcionalidad para soportar updates de precios no cronologicos

--- src/stock_alerter/stock.py
+++ src/stock_alerter/stock.py
@@ -9,16 +9,22 @@ Date = '13/1/18'
 class Stock:
     def __init__(self, symbol):
         self.symbol = symbol
-        self.price_history = []
+        self.price_history = {}
+        self.ordered_timestamps=[]
+
 
     def update(self, timestamp, price):
         if price < 0:
             raise ValueError("price should not be negative")
-        self.price_history.append(price)
+        self.price_history[timestamp]=price
+        self.ordered_timestamps=sorted(list(self.price_history.keys()))
 
     @property
     def price(self):
-        return self.price_history[-1] if self.price_history else None
+        if len(self.price_history)== 0: return None # el diccionario de precios no tiene ninguno
+        return self.price_history[self.ordered_timestamps[-1]]
 
     def is_increasing_trend(self):
-        return self.price_history[-3] < self.price_history[-2] < self.price_history[-1]
+        return self.price_history[self.ordered_timestamps[-3]] \
+               < self.price_history[self.ordered_timestamps[-2]] \
+               < self.price_history[self.ordered_timestamps[-1]]
--- src/stock_alerter/tests/test_stock.py
+++ src/stock_alerter/tests/test_stock.py
@@ -40,7 +40,7 @@ class StockTest(unittest.TestCase):
         pass
 
 
-class StockTrendTest(unittest.TestCase):
+class StockTrendTestOrdered(unittest.TestCase):
 
     def setUp(self):
         self.goog = Stock('GOOG')
@@ -64,6 +64,44 @@ class StockTrendTest(unittest.TestCase):
 
     def tearDown(self):
         pass
+
+
+
+class StockNotOrderedUpdates(unittest.TestCase):
+    def setUp(self):
+        self.goog=Stock('GOOG')
+
+    def test_Stock_price_returns_latest_update(self):
+        self.goog.update(datetime(2014,2,14),10)
+        self.goog.update(datetime(2014, 2, 11), 8)
+        self.goog.update(datetime(2014, 2, 10), 7)
+        self.assertEqual(10,self.goog.price,msg="Stock doesn't returns latest price")
+
+class StockTrendTestNotOrdered(unittest.TestCase):
+
+    def setUp(self):
+        self.goog = Stock('GOOG')
+
+    def given_a_series_of_prices(self, prices):
+        timestamps = [datetime(2014, 2, 13), datetime(2014, 2, 11), datetime(2014, 2, 12)]
+        for timestamp, price in zip(timestamps, prices):
+            self.goog.update(timestamp, price)
+
+    def test_increasing_trend_is_true_if_price_increase_for_3_updates(self):
+        self.given_a_series_of_prices([10, 8, 9])
+        self.assertTrue(self.goog.is_increasing_trend())
+
+    def test_increasing_trend_is_false_if_price_decreases(self):
+        self.given_a_series_of_prices((6, 8, 7))
+        self.assertFalse(self.goog.is_increasing_trend())
+
+    def test_increasing_trend_is_false_if_price_equal(self):
+        self.given_a_series_of_prices((8, 8, 8))
+        self.assertFalse(self.goog.is_increasing_trend())
+
+    def tearDown(self):
+        pass
+
 
 
 if __name__ == '__main__':
